854c331ffb14d890cecfdfbbaa71f4abe9f07812
Logan Young <logany2013@gmail.com>
2018-11-02 13:38:08 -0500

    WIP

--- textreader.py
+++ textreader.py
@@ -16,6 +16,15 @@ def at_least():
                     print(word)
 
 def has_no_e(word):
+    ''' finds words that do not have the letter e
+    >>> has_no_e('potato')
+    True
+    >>> has_no_e('bruther')
+    False
+    >>> has_no_e('jacob')
+    True
+    
+    '''
     if 'e' not in word:
         return True
     else:
@@ -30,10 +39,28 @@ def no_e():
                 num_words += 1
                 if not has_no_e(word):
                   noe += 1
-        print((noe/num_words)*100)
-                          
+        c = (noe/num_words)*100
+        print(f"{c} % of the words do not have e.")
 
+def avoids(word, forbid_lett):
+    for letter in word:
+        if forbid_lett not in word:
+            return True
+        else:
+            return False
+    
+def count_avoids():
+    forbid_letters = input("Enter a list of forbidden letters ")
+    avoid = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if avoids(word, forbid_letters):
+                    avoid += 1
+    print(avoids)
+            
 if __name__ == "__main__":
     #read_file()
     #at_least()
-    no_e()
+    #no_e()
+    count_avoids()
