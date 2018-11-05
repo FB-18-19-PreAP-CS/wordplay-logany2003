225014a9e3b9f22a624b1e8711c88ba194a51d76
Logan Young <logany2013@gmail.com>
2018-11-05 14:12:58 -0600

    WIP

--- textreader.py
+++ textreader.py
@@ -43,11 +43,10 @@ def no_e():
         print(f"{c} % of the words do not have e.")
 
 def avoids(word, forbid_lett):
-    for letter in word:
-        if forbid_lett not in word:
-            return True
-        else:
+    for letter in forbid_lett:
+        if letter.lower() in word.lower():
             return False
+    return True
     
 def count_avoids():
     forbid_letters = input("Enter a list of forbidden letters ")
@@ -57,10 +56,59 @@ def count_avoids():
             for word in line.strip().split():
                 if avoids(word, forbid_letters):
                     avoid += 1
-    print(avoids)
+    print(avoid)
+
+def uses_only(word, letters):
+    for letter in word:
+        if letter.lower() not in letters.lower():
+            return False
+    return True
+
+def words_with_only(letters):
+    uses = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if uses_only(word, letters):
+                    uses += 1
+    print(uses)
             
+def uses_all(word, letters):
+    for letter in letters:
+        if letter.lower() not in word.lower():
+            return False
+    return True
+
+def how_many_uses_all(letters):
+    uses_lett = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if uses_all(word, letters):
+                    uses_lett += 1
+    print(uses_lett)
+
+def is_abecedarian(word):
+    for i in range(len(word)):
+        for j in word[i:]:
+            if word[i].lower() > j.lower():
+                return False
+    return True
+
+def count_abecedarian():
+    count = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                if is_abecedarian(word):
+                    count += 1
+    print(count)
+
 if __name__ == "__main__":
     #read_file()
     #at_least()
     #no_e()
-    count_avoids()
+    #count_avoids()
+    #words_with_only('abcdefg')
+    #how_many_uses_all('aeiou')
+    count_abecedarian()
