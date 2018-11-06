f53929628f2647b6f2dd6e966978cb4101242280
Logan Young <logany2013@gmail.com>
2018-11-06 14:11:07 -0600

    WIP

---
+++ driving.py
@@ -0,0 +1,15 @@
+
+def mile_palin():
+    for i in range(1000000):
+        num = i
+        str(i)
+        if num[2:] == num[5:1:-1]:
+            num = i
+            str(i)
+            if num+1[2:] == num+1[5:1:-1]:
+                num = i
+                str(i)
+                if num+2[2:] == num+2[5:1:-1]:
+                    num = i
+                    str(i)
+                    if num+3[2:] == num+3[5:1:-1]:
